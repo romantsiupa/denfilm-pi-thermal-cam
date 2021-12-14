1ab150cb16b95c12af0d1989f9ff259f0f8b45d6
romantsiupa <roman.tsiupa@eleks.com>
2021-12-14 17:19:34 +0000

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -1,12 +1,44 @@
-import cv2
-# See https://gitlab.com/cvejarano-oss/cmapy/-/blob/master/docs/colorize_all_examples.md to for options that can be put in this list
+# See link to for options that can be put in this list
+# https://gitlab.com/cvejarano-oss/cmapy/-/blob/master/docs/colorize_all_examples.md
 COLORMAP_LIST = ['jet','tab20b','gist_ncar','Set1','prism','tab20b_r','brg_r','bwr','seismic','coolwarm','PiYG_r','tab10','tab20','gnuplot2','brg']
 DEFAULT_COLORMAP_INDEX = 0
 
-# Next Color map
-DISPLAY_INFO = True
+# 0 - Nearest
+# 1 - Inter Linear
+# 2 - Inter Area
+# 3 - Inter Cubic
+# 4 - Inter Lanczos4
+# 5 - Pure Scipy       - nice but slow - 3 fps
+# 6 - Scipy/CV2 Mixed' - nice and fast - 4 fps
+DEFAULT_INTERPOLATION_INDEX = 6
 
-# Interpolation
-INTERPOLATION_LIST = [cv2.INTER_NEAREST,cv2.INTER_LINEAR,cv2.INTER_AREA,cv2.INTER_CUBIC,cv2.INTER_LANCZOS4,5,6]
-INTERPOLATION_LIST_NAME = ['Nearest','Inter Linear','Inter Area','Inter Cubic','Inter Lanczos4','Pure Scipy','Scipy/CV2 Mixed']
-DEFAULT_INTERPOLATION_INDEX = 5
+# Keyboard controll
+CONTROL_COLORMAP_NEXT = "d"
+CONTROL_COLORMAP_PREV = "a"
+CONTROL_INTERPOLATION_NEXT = "w"
+CONTROL_INTERPOLATION_PREV = "s"
+CONTROL_FILTER_ENABLE_DISABLE = "f"
+CONTROL_DISPLAY_INFO = "i"
+
+# !!! DO NOT CHANGE FOLLOWING SETTINGS !!!
+
+# Main monitor index
+MONITOR_INDEX=0
+
+# Display fps, color map name, interpolation algorithm by default
+DISPLAY_INFO_BY_DEFAULT = False
+
+# The speed grades, read more: https://www.i2c-bus.org/speed
+# 100000 - standard mode: 100 kbit/s
+# 400000 - full speed: 400 kbit/s
+# Dont put more
+I2C_FREQUENCY=400000
+
+# If using higher refresh rates yields a 'too many retries' exception, try decreasing this value to work with certain pi/camera combinations
+# 1 = REFRESH_1_HZ
+# 2 = REFRESH_2_HZ
+# 3 = REFRESH_4_HZ
+# 4 = REFRESH_8_HZ
+# 5(TOO MUCH) = REFRESH_16_HZ
+# 6(TOO MUCH) = REFRESH_32_HZ
+CAM_REFRESH_RATE=4
