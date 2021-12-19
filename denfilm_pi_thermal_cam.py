a1a50e7c46055be8e762c6054f15354b79e27074
romantsiupa <roman.tsiupa@eleks.com>
2021-12-19 23:56:44 +0200

    Update denfilm_pi_thermal_cam.py

--- denfilm_pi_thermal_cam.py
+++ denfilm_pi_thermal_cam.py
@@ -221,6 +221,6 @@ if __name__ == "__main__":
     screensize = monitor1.width, monitor1.height
     print(screensize[0])
     print(screensize[1])
-    piautogui.moveTo(screensize[0]-1, screensize[1]-1)
+    pyautogui.moveTo(screensize[0]-1, screensize[1]-1)
     thermcam = pithermalcam()  # Instantiate class
     thermcam.display_camera_onscreen()
